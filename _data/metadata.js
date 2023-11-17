dd776ef1f9d2224a93033def10cd90c22a093e6c
Mr-Richard-B <richbradley90@gmail.com>
2023-11-17 11:40:46 +0000

    updated metadata.js file with my details

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Mr-Richard-B's Eleventy Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my progress to becoming a developer",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Richard Bradley",
+		email: "richbradley90@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
